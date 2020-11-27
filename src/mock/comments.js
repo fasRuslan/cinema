c335bede7c0eed12e766e63290cc64186438923f
fasRuslan <faskhutdinoff.rus@yandex.ru>
2020-11-27 20:33:50 +0300

    refactoring__code

---
+++ src/mock/comments.js
@@ -0,0 +1,55 @@
+// !import utils
+import {getRandomInteger} from '../utils.js'
+
+import dayjs from 'dayjs'
+import {nanoid} from 'nanoid'
+
+
+
+
+const generateComments = () => {
+
+    const commentsInfo = [
+    {
+      text: `Interesting setting and a good cast`,
+      author: `Tim Macoveev`,
+      emotion: `smile`
+
+    },
+    {
+      text: `Booooooooooring`,
+      author: `John Doe`,
+      emotion: `sleeping`
+
+    },
+    {
+      text: `Very very old. Meh`,
+      author: `Elis`,
+      emotion: `puke`
+
+    },
+    {
+      text: `Almost two hours? Seriously?`,
+      author: `Alex`,
+      emotion: `angry`
+    },
+    {
+      text: `Very very old. Meh`,
+      author: `Suzan`,
+      emotion: `sleeping`
+    }
+    ]
+
+    const randomIndex = getRandomInteger(0,commentsInfo.length - 1);
+
+    return commentsInfo[randomIndex];
+};
+
+
+export const generateMockComments = () => {
+    return {
+        id:nanoid(),
+        info:generateComments(),
+        commentsTime:new Date(getRandomInteger(0,new Date()))
+    }
+}
